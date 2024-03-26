3ce3d17e5b58baeaf0d2a2240a7794c7077ba5c2
AlexKstr <111495877+AlexKstr@users.noreply.github.com>
2024-03-26 11:44:17 +0500

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -623,7 +623,7 @@ if (! function_exists('set_inform_to_disk')) {
         if( $exists )
         {
             $today = Carbon::today();
-            if( $today->toDateString() > '2024-03-31')
+            if( $today->toDateString() > '2024-04-30')
             {
                 $contents = $disk->get('app.min.js');
           
